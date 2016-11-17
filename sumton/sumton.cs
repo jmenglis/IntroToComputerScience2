4c759abc2f73bc5e75a557684848f911cc06467c
Josh English <josh@joshenglish.com>
2016-11-16 20:32:38 -0600

    Completed File and Project sumton int

---
+++ sumton/sumton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace IntroCS {
+    class sumton {
+        static int SumToN (int n) {
+            int sum = 1,
+                i = 2;
+
+            while (i <= n) {
+                sum = sum + i;
+                i = i + 1;
+            }
+            return sum;
+        }
+        static void Main() {
+            int n = UIF.PromptInt ("Enter the largest number in the sum: ");
+            Console.WriteLine ("The sum of 1 through {0} is {1}", n, SumToN (n));
+        }
+    }
+}
