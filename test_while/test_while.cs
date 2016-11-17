5ecfd0330ec8b3404790d6656185d384929c7c72
Josh English <josh@joshenglish.com>
2016-11-16 20:06:34 -0600

    Completing Test While

---
+++ test_while/test_while.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class test_while {
+    static void Main() {
+        int i = 4;
+        while (i < 9) {
+            Console.WriteLine (i);
+            i = i + 2;
+        }
+    }
+}
---
+++ test_while2/test_while2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class test_while2 {
+    static void Main() {
+        int i = 4;
+        while (i < 9) {
+            i = i + 2;
+            Console.WriteLine (i);
+        }
+    }
+}
