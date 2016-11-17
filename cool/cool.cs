6e4a1a76b4dafbcb0c68b001994122af2592a885
Josh English <josh@joshenglish.com>
2016-11-16 19:55:15 -0600

    First project

---
+++ cool/cool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Cool {
+    static void Main() {
+        int temperature = 115;
+        while (temperature > 112) {
+            Console.WriteLine (temperature);
+            temperature = temperature - 1;
+
+        }
+        Console.WriteLine ("The tea is cool enough");
+    }
+}
