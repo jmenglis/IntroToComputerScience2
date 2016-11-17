ee01046ba913617ad26f6efde600453c8b553723
Josh English <josh@joshenglish.com>
2016-11-16 20:09:38 -0600

    Adding blastoff.cs

---
+++ blastoff/blastoff.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class blastoff {
+    static void Main() {
+        int i = 10;
+        while (i > 0) {
+            Console.WriteLine (i);
+            i = i - 1;
+        }
+        Console.WriteLine ("blastoff!");
+    }
+}
