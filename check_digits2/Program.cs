fabf8986d6a634ac165ee796c31a02af4210317a
Josh English <josh@joshenglish.com>
2016-12-10 14:55:46 -0600

    Completed check_digits2 and loop_steps

---
+++ check_digits2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IntroCS {
+	class CheckDigits {
+		static void Main() {
+			string s = UIF.PromptLine("Enter a line: ");
+			Console.WriteLine(IsDigits(s));
+		}
+		static bool IsDigits(string s) {
+			int i = 0;
+			while (i < s.Length) {
+				if(s[i] < '0' || s[i] > '9') {
+					return false;
+				}
+				i++;
+			}
+			return (s.Length > 0);
+		}
+	}
+}
---
+++ loop_steps/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IntroCS {
+	class LoopSteps {
+		static void Main() {
+			string s = "abcd";
+			int i = 1;
+			while (i < 4) {
+				Console.Write("/" + s[i] + s[i - 1]);
+				i++;
+			}
+			Console.WriteLine();
+		}
+	}
+}
