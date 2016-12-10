cb9c1b1a439a25a0fd321fd3163cf8c87c59eed5
Josh English <josh@joshenglish.com>
2016-12-10 11:48:19 -0600

    Catching this up

---
+++ sum_to_n_long_quick/sum_to_n_test_long.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IntroCS {
+	class SumToNTest {
+		static long SumToN(int n) {
+			return (long)n * (n + 1) / 2;
+		}
+
+		static void Main() {
+			int n = UIF.PromptInt("Enter the largest number in the sum: ");
+			Console.WriteLine("The sum of 1 through {0} is {1}", n, SumToN(n));
+		}
+	}
+}
---
+++ sum_to_n_test_long/sum_to_n_test_long.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntroCS {
+	class SumToNTest {
+		static long SumToN(int n) {
+			long sum = 1, i = 2;
+			while (i <= n) {
+				sum = sum + i;
+				i = i + 1;
+			}
+			return sum;
+		}
+
+		static void Main() {
+			int n = UIF.PromptInt("Enter the largest number in the sum: ");
+			Console.WriteLine("The Sum of 1 through {0} is {1}.", n, SumToN(n));
+		}
+	}
+}
