a9826a19dce72a700f0e45c82b62764c3e183ef6
Josh English <josh@joshenglish.com>
2016-12-10 12:04:40 -0600

    Finished char_loop1.cs

---
+++ char_loop1/char_loop1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroCS {
+	class CharLoop {
+		static void Main() {
+			string s = UIF.PromptLine("Enter a line: ");
+			OneCharPerLine(s);
+		}
+		static void OneCharPerLine(string s) {
+			int i = 0;
+			while (i < s.Length) {
+				Console.WriteLine(s[i]);
+				i++;
+			}
+		}
+	}
+}
