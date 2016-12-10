8a4699611a3d6c1d85e52283d3d99f8fb66f185f
Josh English <josh@joshenglish.com>
2016-12-10 14:39:14 -0600

    Finishing the check_digits1

---
+++ check_digits1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntroCS {
+	class CheckDigits {
+		static void Main() {
+			string s = UIF.PromptLine("Enter a line: ");
+			Console.WriteLine(IsDigits(s));
+		}
+		static bool IsDigits(string s) {
+			bool allDigitsSoFar = (s.Length > 0);
+			int i = 0;
+			while (i < s.Length) {
+				if (s[i] < '0' || s[i] > '9') {
+					allDigitsSoFar = false;
+				}
+				i++;
+			}
+			return allDigitsSoFar;
+		}
+	}
+}
