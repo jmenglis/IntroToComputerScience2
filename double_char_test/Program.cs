8f1cd0c66b2c30ba744600533f50848362c8dc86
Josh English <josh@joshenglish.com>
2016-12-10 16:44:22 -0600

    Adding the finishing of 6.2.4.1

---
+++ double_char_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IntroCS {
+	class DoubleCharTest {
+		static void Main() {
+			string s = UIF.PromptLine("Enter Line Here: ");
+			Console.Write(HasDoubleChar(s));
+		}
+		static bool HasDoubleChar(string s) {
+			int i = 1;
+			while (i < s.Length) {
+				if(s[i] == s[i - 1]) {
+					return true;
+				}
+				i++;
+			}
+			return false;
+		}
+	}
+}
